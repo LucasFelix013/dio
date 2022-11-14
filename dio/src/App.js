4204e87154bc363f565f5089e62ef47c91984cf5
LucasFelix013 <luquinhas.pgg@gmail.com>
2022-11-14 00:13:17 -0300

    first commit

---
+++ dio/src/App.js
@@ -0,0 +1,14 @@
+import Button from './components/button';
+
+function App() {
+  return ( 
+     <div className="App">
+     <h1>Olá React</h1>
+     <Button title="Entra"/>
+     
+
+     </div> 
+     );
+}
+
+export default App;
